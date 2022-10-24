fc1e066058e7cf09b27dec1a86f38867d9851973
CristinaFeijoo <thatfoxy124@gmail.com>
2022-10-24 13:41:32 -0500

    first commit

--- Contenido.java
+++ Contenido.java
@@ -1,3 +1,34 @@
 public class Contenido {
-    System.Out.Println
+    public static void main(String[] args){
+        System.Out.Print ("20 conceptos básicos de programación")
+        System.Out.Println ("Algoritmo");
+        System.Out.Println ("Codigo");
+        System.Out.Println ("Lenguaje");
+        System.Out.Println ("Codigo Fuente");
+        System.Out.Println ("Codigo Maquina");
+        System.Out.Println ("Codigo Binario");
+        System.Out.Println ("Bug");
+        System.Out.Println ("Refactorizacion");
+        System.Out.Println ("Frameworks");
+        System.Out.Println ("Queries");
+        System.Out.Println ("Front-End");
+        System.Out.Println ("Back-End");
+        System.Out.Println ("Full-Stack");
+        System.Out.Println ("SLL");
+        System.Out.Println ("SOLID");
+        System.Out.Println ("Backdoor");
+        System.Out.Println ("Backup");
+        System.Out.Println ("Bandwidth");
+        System.Out.Println ("Big data");
+        System.Out.Println ("BIOS");
+
+
+
+
+
+
+
+
+
+    }
 }
