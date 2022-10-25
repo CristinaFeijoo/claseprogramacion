25f9c4c87bad054d85b5b81e47e26c292ce629b2
CristinaFeijoo <thatfoxy124@gmail.com>
2022-10-24 22:08:31 -0500

    Git

---
+++ Git.java
@@ -0,0 +1,12 @@
+public class Git {
+    public static void main (String[] args){
+        System.Out.Print ("comandos git")
+        System.Out.Print ("git config --global user email :se usa para configurar en el sistema la cuenta de github mediante el correo")
+        System.Out.Print ("git config --global user.name : se usa de manera de configurar el sistema el git con el nombre de usurio")
+        System.Out.Print ("git branch  Podemos usar el comando git branch para crear, enumerar y eliminar ramas")
+        System.Out.Print ("git commit Una vez que llegamos a cierto punto en el desarrollo, queremos guardar nuestros cambios ")
+        System.Out.Print ("git push  enviar estos cambios al servidor remoto")
+        System.Out.Print ("git main origin  enviar estos cambios al reporsitorio main del codigo o archivo principal")
+        System.Out.Print ("git init creará un nuevo repositorio local GIT. El siguiente comando de Git creará un repositorio en el directorio actual")
+    }
+}
